229cea44510f3e2282260c4d6753ac0d65afaa3f
kei-yagisawa <till.angelus@gmail.com>
2022-09-12 10:57:23 +0900

    * `panic_halt`の使用 * ダミーループを`asm!("nop")`に変更

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,40 @@
 #![no_main]
 #![no_std]
-#![feature(llvm_asm)]
+#![feature(asm)]
+
+extern crate panic_halt;
+
 
 #[link_section = ".vector_table.reset_vector"]
 #[no_mangle]
 pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
 
 #[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
+pub extern "C" fn Reset() -> ! {
     let rcc_ahb1enr = 0x4002_3830 as *mut usize;
     let gpioa_moder = 0x4002_0000 as *mut usize;
     let gpioa_odr   = 0x4002_0014 as *mut usize;
 
     set_bit(rcc_ahb1enr,  0, true);
     set_bit(gpioa_moder, 10, true);
-
+    
     let mut flg: bool = true;
     loop {
         flg = !flg;
         set_bit(gpioa_odr, 5, flg);
 
         for _ in 0..24_000 {
-            llvm_asm!("" :::: "volatile")
+            unsafe { asm!("nop"); }
         }
     }
 }
 
-unsafe fn set_bit( addr: *mut usize, bit: usize, is_set: bool ) {
-    if is_set {
-        *addr |= 1 << bit;
-    }
-    else {
-        *addr &= !(1 << bit);
-    }
-}
 
-
-use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
+fn set_bit( addr: *mut usize, bit: usize, is_set: bool ) {
+    unsafe {
+        match is_set {
+            true  => *addr |= 1 << bit,
+            false => *addr &= !(1 << bit)
+        }
+    }
 }
