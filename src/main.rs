6da3073f55ddee6910a68606df68b170e5782608
kei-yagisawa <till.angelus@gmail.com>
2022-09-25 12:47:58 +0900

    `SysTick`を構造体に

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,18 @@ use nucleo_blinky_startup::systick;
 pub fn main() -> ! {
     led::init();
 
-    systick::init(|| {
+    let mut st = systick::SysTick::new();
+    st.set_current(0);
+    st.set_reload(1_000_000);
+    st.set_interrupt(Some(|| {
         static mut ON_OFF: bool = false;
-    
+
         unsafe {
             ON_OFF = !ON_OFF;
             led::set(ON_OFF);
         }
-    });
+    }));
+    st.start();
 
     loop {}
 }
--- src/systick.rs
+++ src/systick.rs
@@ -1,25 +1,73 @@
-use core::ptr::write_volatile;
+use volatile_register::{RO, RW};
+
+#[repr(C)]
+struct SysTickReg {
+    pub csr:   RW<u32>,
+    pub rvr:   RW<u32>,
+    pub cvr:   RW<u32>,
+    pub calib: RO<u32>,
+}
+
+pub struct SysTick {
+    reg: &'static mut SysTickReg,
+}
+
+impl SysTick {
+    pub fn new() -> SysTick {
+        SysTick {
+            reg: unsafe { &mut *(0xE000_E010 as *mut SysTickReg) },
+        }
+    }
+
+    pub fn start(&mut self) {
+        unsafe {
+            let csr = self.reg.csr.read();
+            self.reg.csr.write(csr | 0x1);
+        }
+    }
+
+    pub fn stop(&mut self) {
+        unsafe {
+            let csr = self.reg.csr.read();
+            self.reg.csr.write(csr & !0x1);
+        }
+    }
+
+    pub fn set_interrupt(&mut self, cb: Option<fn()>) {
+        unsafe {
+            CB = cb;
+
+            let csr = self.reg.csr.read();
+            match cb {
+                Some(_) => self.reg.csr.write(csr |  0x2),
+                None    => self.reg.csr.write(csr & !0x2),
+            }
+        }
+    }
+
+    pub fn get_current(&self) -> u32 {
+        self.reg.cvr.read()
+    }
+
+    pub fn set_current(&mut self, current_value: u32) {
+        unsafe {
+            self.reg.cvr.write(current_value);
+        }
+    }
+
+    pub fn set_reload(&mut self, reload_value: u32) {
+        unsafe {
+            self.reg.rvr.write(reload_value);
+        }
+    }
+}
 
 static mut CB: Option<fn()> = None;
 
 #[no_mangle]
-pub extern "C" fn SysTick() {
-    match unsafe { CB } {
+pub unsafe extern "C" fn SysTick() {
+    match CB {
         Some(f) => f(),
-        None    => ()
-    }
-}
-
-pub fn init( f: fn() ) {
-    unsafe {
-        CB = Some(f);
-    
-        let stk_ctrl_addr = 0xE000_E010 as *mut usize;
-        let stk_load_addr = 0xE000_E014 as *mut usize;
-        let stk_val_addr  = 0xE000_E018 as *mut usize;
-
-        write_volatile(stk_val_addr, 0);
-        write_volatile(stk_load_addr, 1_000_000);
-        write_volatile(stk_ctrl_addr, 0x3);
+        None    => (),
     }
 }
